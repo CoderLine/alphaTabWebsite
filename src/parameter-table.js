6201292dd7317b80217981faa076a5c49017ae85
Danielku15 <danielku15@coderline.net>
2021-12-29 18:32:42 +0100

    Add docs for Lazy Loading extension

--- src/parameter-table.js
+++ src/parameter-table.js
@@ -24,7 +24,7 @@ export function ParameterRow({ children, type, platform, name }) {
       <td>
         {Array.isArray(children)
           ? children.map((c) => renderChild(c))
-          : children}
+          : renderChild(children)}
       </td>
     </tr>
   );
