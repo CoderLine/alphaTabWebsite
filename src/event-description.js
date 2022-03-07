de529bca57c53aa85ea342a86d43c9761e58a607
Danielku15 <danielku15@coderline.net>
2022-03-07 21:18:27 +0100

    Added docs for new playbackRangeChanged

--- src/event-description.js
+++ src/event-description.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import {Page} from './page'
 import CodeBadge from './code-badge'
+import { buildNames } from './names';
 
 export default function EventDescription({metadata}) {
     const page = new Page(metadata);
-    const dotNetName = page.prop("title");
-    const jsNames = page.props("jsName");
-    const domNames = page.props("domName");
+    const { jsNames, csNames, jQueryNames, domNames } = buildNames(page);
     return (
         <table className="table table-striped table-condensed type-table">
             <tbody>
-                <tr>
-                    <td><CodeBadge type="net" name={dotNetName} /></td>
-                </tr>
                 {jsNames.map((name, index) => (
                     <tr key={index}>
                         <td><CodeBadge type="js" name={name} /></td>
                     </tr>
-                ))}
+                ))}                
+                {jQueryNames.map((name, index) => (
+                    <tr key={index}>
+                        <td><CodeBadge type="jquery" name={name} /></td>
+                    </tr>
+                ))}            
                 {domNames.map((name, index) => (
                     <tr key={index}>
                         <td><CodeBadge type="html" name={name} /></td>
+                    </tr>
+                ))}
+                {csNames.map((name, index) => (
+                    <tr key={index}>
+                        <td><CodeBadge type="net" name={name} /></td>
                     </tr>
                 ))}
             </tbody>
