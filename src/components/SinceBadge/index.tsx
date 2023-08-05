c8c08804eca8e8a503ec2c69c7b1703b7ce99ca3
Danielku15 <danielku15@coderline.net>
2023-08-05 22:02:04 +0200

    Update docs

--- src/components/SinceBadge/index.tsx
+++ src/components/SinceBadge/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-export class SinceBadge extends React.Component<{since: string}> {
+export class SinceBadge extends React.Component<{ since: string, inline?: boolean }> {
     public render() {
-        return (
-            <span className={`badge badge--info ${styles.since}`}>since {this.props.since}</span>
-        )
+        if (this.props.since) {
+            return (
+                <span className={`badge badge--info ${styles.since} ${this.props.inline ? styles.sinceInline : ''}`}>since {this.props.since}</span>
+            )
+        } else {
+            return (<></>);
+        }
     }
 }
